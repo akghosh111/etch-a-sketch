445bf439f3c9ee605153b57445594f16513744a4
akghosh111 <ghoshkiran2017@gmail.com>
2024-03-31 00:05:42 +0530

    added click only hover

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 let color = "black";
+let click = false;
 
 document.addEventListener("DOMContentLoaded", function () {
 
     createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function (e) {
+        if (e.target.tagName != "BUTTON") {
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if (click) {
+                draw.innerHTML = "You're allowed to draw"
+            }
+            else {
+                draw.innerHTML = "You're not allowed to draw"
+            }
+
+        }
+    })
 
     let btn_popup = document.querySelector("#popup");
     btn_popup.addEventListener("click", function () {
@@ -45,13 +60,17 @@ function getSize() {
 }
 
 function colorDiv() {
-    if (color == "random") {
-        this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`
+    if (click) {
+        if (color == "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
 
 
-    }
-    else {
-        this.style.backgroundColor = 'black'
+        }
+        else {
+            this.style.backgroundColor = 'black'
+        }
+
+
     }
 
 }
@@ -61,7 +80,7 @@ function setColor(colorChoice) {
 }
 
 
-function resetBoard(){
+function resetBoard() {
     let divs = document.querySelectorAll("div")
     divs.forEach((div) => div.style.backgroundColor = "white")
 }
