63a1ba5fb05a2500e61f37a544e14970fb24f4e9
akghosh111 <ghoshkiran2017@gmail.com>
2024-03-30 23:54:22 +0530

    added resetBoard

--- script.js
+++ script.js
@@ -59,3 +59,9 @@ function colorDiv() {
 function setColor(colorChoice) {
     color = colorChoice;
 }
+
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor = "white")
+}
